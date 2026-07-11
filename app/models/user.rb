0084043fd38f3fbbf9689902b4b7edb6c2983a9c
Itsuki Nishijima <itsuki1108864@gmail.com>
2026-07-11 16:46:26 +0900

    -プロフィールの編集ができるようんした（cssも整えた）

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,15 @@ class User < ApplicationRecord
 
   has_many :boards, dependent: :destroy
   has_many :tasks, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
-  has_many :comments
+  has_one_attached :avatar
+
+  validates :name,
+            presence: true,
+            length: { maximum: 20 }
+
+  validates :profile,
+            length: { maximum: 300 },
+            allow_blank: true
 end
